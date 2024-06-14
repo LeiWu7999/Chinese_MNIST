f2981ddb844827d756651a85fdc7074c5641dfa8
unknown <wyf2930174667@gmail.com>
2024-06-14 13:35:26 +0800

    6/14/13:35

--- utils/networkHelper.py
+++ utils/networkHelper.py
@@ -9,3 +9,11 @@ def extract(tensor, idx, x_shape):
     batch_size = idx.shape[0]
     out = tensor.gather(-1, idx.cpu())
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(idx.device)
+
+def exists(x):
+    """
+    判断数值是否为空
+    :param x: 输入数据
+    :return: 如果不为空则True 反之则返回False
+    """
+    return x is not None
